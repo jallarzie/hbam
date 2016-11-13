b003072ff0240b9dc7c8a259d85c915637a49f97
g-drol <guyd1491@hotmail.fr>
2016-11-13 13:38:53 -0500

    Main Menu Scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public string sceneToLoad;
+
+	public void LoadScene(string loadedScene){
+		SceneManager.LoadScene (loadedScene, LoadSceneMode.Single);
+	}
+}
